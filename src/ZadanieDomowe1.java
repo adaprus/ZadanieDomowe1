48be1414df63cae79cbbebe697a113505ec25e8f
Prus <ada.olga.prus@gmail.com>
2019-07-02 22:13:56 +0200

    Dopisałam od kiedy się uczę

--- src/ZadanieDomowe1.java
+++ src/ZadanieDomowe1.java
@@ -1,6 +1,7 @@
 public class ZadanieDomowe1 {
     public static void main(String[] args) {
         System.out.println("Uczę się programowania i commitowania");
+        System.out.println("Rozpoczęłam kurs 1 lipca 2019");
 
     }
 }
